d6bc2882e3cc4682a6d14aa26784069d5ef7e016
kapil jhajhria <jhajhria44@gmail.com>
2019-10-22 10:42:58 +0530

    fade transition

--- lib/main.dart
+++ lib/main.dart
@@ -19,17 +19,46 @@ class AnimatedButton extends StatefulWidget {
   _AnimatedButtonState createState() => _AnimatedButtonState();
 }
 
-class _AnimatedButtonState extends State<AnimatedButton> {
+class _AnimatedButtonState extends State<AnimatedButton>
+    with SingleTickerProviderStateMixin {
+  AnimationController myController;
+  double locationBtnOpacity = 1.0;
 
+  @override
+  void initState() {
+    myController =
+        AnimationController(duration: Duration(milliseconds: 500), vsync: this);
+    //add status listener to get blinking effect
+    myController.addStatusListener((AnimationStatus buttonAnimationStatus) {
+      if (buttonAnimationStatus == AnimationStatus.completed) {
+        myController.reverse();
+      } else if (buttonAnimationStatus == AnimationStatus.dismissed) {
+        myController.forward();
+      }
+    });
+    // TODO: implement initState
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: toggleVisible,
-      child: Icon(
-        Icons.location_on,
-        color: Colors.white,
-        size: 60,
+      onTap: () {
+        myController.forward();
+        setState(() {});
+      },
+      child: Container(
+        height: 150,
+        width: 150,
+        color: Colors.grey,
+        child: FadeTransition(
+          opacity: myController,
+          child: Icon(
+            Icons.location_on,
+            color: Colors.white,
+            size: 60,
+          ),
+        ),
       ),
     );
   }
